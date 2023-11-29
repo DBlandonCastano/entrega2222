5f6c20f8d9418b12be55dfa2b698c3d993a3e9d1
Durlandy Blandón <durlandyb@gmail.com>
2023-11-28 19:58:30 -0500

    organized

--- src/component/WeatherDetaill.jsx
+++ src/component/WeatherDetaill.jsx
@@ -11,43 +11,43 @@ const WeatherDetaill = ({ weather }) => {
     return farenheit;
   }
   return (
-    <article className='text-center grid gap-6p sm:text-center'>
-      <h3 className='text-3xl text-black m-5 sm:text-5xl'>{weather.name},{weather.sys.country} </h3>
+    <article className='text-center grid gap-8 text-center sm:text-center'>
+      <h3 className='text-2xl text-black m-3'><b>{weather.name},{weather.sys.country} </b>  </h3>
       {/* seccion temperatura, descripcion e imagen */}
-      <div className='text-black grid gap-6 sm:grid sm:grid-cols-2'>
-        <section className='grid grid-cols-2 h-60 bg-white/60 p-2 rounded-xl justify-items-center items-center sm:h-96 sm:justify-center sm:text-center' >
-          <h3 className='col-span-2 text-xl capitalize sm:text-4xl sm:col-span-2 sm:p-4'>{weather.weather[0].description} </h3>
-          <span className='text-3xl sm:text-5xl'>{celsius ? (`${weather.main.temp} °C`) : (celsiusToFarenheit(weather.main.temp)) + '°F'}</span>
+      <div className='text-black grid gap-6 sm:grid sm:grid-cols-[65%,35%] sm:text-2xl'>
+        <section className='grid grid-cols-2 h-[200px] bg-white/60 p-2 rounded-xl justify-items-center items-center sm:justify-center sm:text-center sm:h-[250px] ' >
+          <h3 className='col-span-2  text-[18px] capitalize  sm:col-span-2 sm:p-4 sm:text-[18px]'><strong>  {weather.weather[0].description}</strong> </h3>
+          <span className='text-[20px]'> <b> {celsius ? (`${weather.main.temp} °C`) : (celsiusToFarenheit(weather.main.temp)) + '°F'}</b></span>
           <div>
             <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
           </div>
         </section>
         {/* seccion detalles temperatura */}
-        <section className='grid grid-cols-3 gap-5 justify-items-center bg-white/60 p-2 rounded-xl sm:flex sm:flex-col sm:justify-center sm:text-4xl sm:gap-12 sm:p-16'   >
+        <section className='grid grid-cols-3 gap-2 justify-items-center bg-white/60 p-2 rounded-xl sm:flex sm:flex-col sm:justify-center sm:text-[14px] sm:gap-5 '   >
           <div className='flex gap-2'>
             <div>
               <img src="/images/ventoso.png" alt="Viento" />
             </div>
-            <span>{weather.wind.speed} m/s</span>
+            <span> <strong>{weather.wind.speed} m/s</strong></span>
           </div>
           <div className='flex gap-2'>
             <div>
               <img src="/images/dos.png" alt="Humedad" />
             </div>
-            <span>{weather.main.humidity} %</span>
+            <span><strong>{weather.main.humidity}</strong> %</span>
           </div>
           <div className='flex gap-2'>
             <div className='flex'>
               <img src="/images/humedad.png" alt="Presurización" />
               <img src="/images/humedad.png" alt="Presurización" />
             </div>
-            <span>{weather.main.pressure} hPa</span>
+            <span><strong>{weather.main.pressure} hPa</strong></span>
           </div>
         </section>
       </div>
 
-      <button onClick={handleCF}className='bg-white/60 border-3 text-black w-25 m-6 p-1 text-2x border-solid rounded-3xl hover:bg-sky-700 transition-colors sm:text-5xl sm:h-5xl sm:hover:items-center sh:m-3xl'>
-        {celsius ? "Farenheit" : "Centigrados"}
+      <button onClick={handleCF}className='top-20  bg-white/60  border-1  m-[0_auto] text-black p-3 text-[12px] border-solid rounded-3xl hover:bg-sky-700 transition-colors sm:text-[16px] sm:h-[45px] sm:hover:items-center sm:p- '>
+        <b>{celsius ? "Farenheit" : "Centigrados"} </b>
       </button>
     </article>
   )
